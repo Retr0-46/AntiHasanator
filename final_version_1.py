bc6162b81f26e48713a6ae772d7a93d7b71532da
vkv1004 <75637047+vkv1004@users.noreply.github.com>
2022-05-21 10:28:33 +0300

    1

--- final_version_1.py
+++ final_version_1.py
@@ -1,22 +1,23 @@
-#This part of code must allocate all red objects
-#Firstly we import all necessary libraries
 import numpy as np
 import cv2
 
 #then input picture and change color space
 
-frame1 = cv2.imread('vectorpaint.png.png', 1)
+frame1 = cv2.imread('0.png', 1)
 median = cv2.medianBlur(frame1,5)
 img3=frame1.copy()
 img4=frame1.copy()
+img_hsv = frame1.copy()
 
 frame = cv2.cvtColor(median, cv2.COLOR_BGR2HSV)
 #here I screwed up with color ranges but this red mask probably working
-hsv_min = np.array((0, 100,40), np.uint8)
+
+hsv_min = np.array((0, 80, 80), np.uint8)
 hsv_max = np.array((10, 255, 255), np.uint8)
 
-thresh = cv2.inRange(frame, hsv_min, hsv_max)
-img =cv2.inRange(frame, hsv_min, hsv_max)
+# upper boundary RED color range values; Hue (160 - 180)
+thresh = cv2.inRange(frame, hsv_min,hsv_max)
+img =cv2.inRange(frame, hsv_min,hsv_max)
 
 contours_r, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 #this thing output all red centroids
@@ -28,7 +29,7 @@ try:
         x_centre = left_up[0] + round((right_up[0] - left_up[0]) / 2)
         y_centre = right_up[1] + round((right_down[1] - right_up[1]) / 2)
         red_shapes.append([x_centre, y_centre])
-        # print(left_up, left_down, right_down, right_up)
+        print(left_up, left_down, right_down, right_up)
 except:
     pass
 
@@ -43,7 +44,7 @@ output = [red_shapes]
 try:
     #сортирую по игрику и удаляю координаты светофора
     u=sorted(red_shapes,key=lambda y: y[1])
-    print(u)
+    print('red obj'+u)
 
     u1=[]
     u1.extend(u)
@@ -67,7 +68,7 @@ plates = plates_cascade.detectMultiScale(gray, 1.2, 4)
 
 try:
     for (x, y, w, h) in plates:
-        print(x,y)
+        print('n', x,y)
 
         plates_rec = cv2.rectangle(img3, (x, y), (x + w, y + h), (0, 255, 0), 2)
 except:
@@ -99,9 +100,11 @@ except:
     pass
 
 u3=sorted(blue_shapes,key=lambda y: y[1])
-print(u)
+print(u3)
 
 if (len(u3))==2 and (len(u1))>=2 and t=='True':
     print('Нарушения нет')
+elif (len(u3)==1 and (len(u1))>=2 and t=='True'):
+    print('Нарушение')
 else:
-    print('Нарушение обнаружено')
+    print('Затрудняюсь ответить')
